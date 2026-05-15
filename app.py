b0748a5d19059cff5edf8137f7f1b677b65eb6aa
TomasMacula <tomasmacula@live.com>
2026-05-15 11:39:45 +0200

    konfiguracia prostredia a sprava zavislosti - requirements.txt - .gitignore - .env

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, jsonify
 from database import db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://neondb_owner:npg_J4KYfHNULD5O@ep-floral-dream-alrhxd2c-pooler.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 @app.route("/")
-def hello_world():
-    return "sprava kniznice"
+def home():
+    return "E-shop API"
+
+if __name__ == '__main__':
+    app.run(debug=True)
